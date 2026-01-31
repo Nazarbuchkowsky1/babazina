82bd239a829aec41e3a093b58428c2bf70a65626
Nazarbuchkowsky1 <binancedetrauder@gmail.com>
2026-01-31 19:09:21 +0100

    Delete convert_image.js

--- convert_image.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const file = 'home.jpg';
-const data = fs.readFileSync(file);
-const base64 = data.toString('base64');
-fs.writeFileSync('base64_image.txt', 'data:image/jpeg;base64,' + base64);
-console.log('Conversion complete. Check base64_image.txt');
